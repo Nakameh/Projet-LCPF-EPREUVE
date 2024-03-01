ad4bb331ddd7836e3f8349660977cef4087d9070
Noah HENOCH <henoch.noah@gmail.com>
2024-03-01 11:21:32 +0100

    Question 3 et 4

--- CPL.hs
+++ CPL.hs
@@ -27,3 +27,23 @@ w1 = ["t1", "t2"]
 
 w2 :: World
 w2 = ["p1", "p2", "t1", "t2"]
+
+genAllWorlds :: [String] -> [World]
+genAllWorlds [] = [[]]
+genAllWorlds (x:xs) = map (x:) gaw ++ gaw
+    where gaw = genAllWorlds xs
+
+sat :: World -> Formula -> Bool
+sat w T = True
+sat w F = False
+sat w (And f1 f2) = sat w f1 && sat w f2
+sat w (Or f1 f2) = sat w f1 || sat w f2
+sat w (Imp f1 f2) = not (sat w f1) || sat w f2
+sat w (Eqv f1 f2) = sat w f1 == sat w f2
+sat w (Not f) = not (sat w f)
+sat w (Var x) = contient w x
+    where 
+        contient [] _ = False
+        contient (y:ys) x 
+            | x == y = True
+            | otherwise = contient ys x
